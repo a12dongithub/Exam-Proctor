482aea8bd5781b84a766f551c2bd33321b6fc962
varangrai <varangrai@gmail.com>
2021-10-30 09:48:33 +0530

    Train_test_files

---
+++ Auto-ProctorDL/YOLO/test.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)

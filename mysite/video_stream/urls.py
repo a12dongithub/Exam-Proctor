e8243c29b87f83935deb79044eab79e399fdf582
souravpal16 <souravpal.4916@gmail.com>
2021-10-31 09:56:54 +0530

    taskMan to server connection estabished

--- mysite/video_stream/urls.py
+++ mysite/video_stream/urls.py
@@ -4,7 +4,8 @@ from video_stream import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('video_feed', views.video_feed, name='video_feed')
+    path('video_feed', views.video_feed, name='video_feed'),
+    path('detect/', views.detect, name="detect")
     # path('webcam_feed', views.webcam_feed, name='webcam_feed'),
     # path('mask_feed', views.mask_feed, name='mask_feed'),
 	# path('livecam_feed', views.livecam_feed, name='livecam_feed'),
--- mysite/video_stream/views.py
+++ mysite/video_stream/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
 from video_stream.camera import VideoCamera
 # Create your views here.
-
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 def index(request):
 	return render(request, 'video_stream/home.html')
@@ -24,7 +25,15 @@ def video_feed(request):
 # 	return StreamingHttpResponse(gen(IPWebCam()),
 # 					content_type='multipart/x-mixed-replace; boundary=frame')
 
-
+@csrf_exempt
+def detect(request):
+    # if request.method == 'GET':
+    if request.method == 'POST':
+        time = request.POST['time']
+        program_name = request.POST['program_name']
+        #return HttpResponse(f'The process found running is: {program_name}: {time}')
+        print(f'{program_name}: {time}')
+    return HttpResponse("Listening for cheating programs...")
 # def mask_feed(request):
 # 	return StreamingHttpResponse(gen(MaskDetect()),
 # 					content_type='multipart/x-mixed-replace; boundary=frame')
